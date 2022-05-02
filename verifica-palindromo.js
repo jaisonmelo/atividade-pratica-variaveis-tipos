19f28741f3f34c675152addcfda3d73488671026
jaisonmelo <35084196+jaisonmelo@users.noreply.github.com>
2022-05-02 20:37:28 -0300

    função palindromo

---
+++ verifica-palindromo.js
@@ -0,0 +1,19 @@
+//solição 01
+function verificaPalindromo(string) {
+    if(!string) return;
+
+    return string.split("").reverse().join("") === string;
+}
+function verificaPalindromo2(string) {
+    if(!string) return;
+
+    for(let i = 0; i < string.length /2; i++) {
+        if(string[i] !== string[string.length -1 -i]){
+            return false
+        }
+    }
+
+    return true;
+}
+
+console.log(verificaPalindromo2("ama"))
